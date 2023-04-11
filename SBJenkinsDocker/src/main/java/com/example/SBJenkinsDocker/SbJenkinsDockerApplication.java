6235d72ee004c746bdfa67e7ef5b70f1af5b0317
jenikinsdocker <jenikinsdocker@gmail.com>
2023-04-11 11:54:14 +0530

    project structure added

---
+++ SBJenkinsDocker/src/main/java/com/example/SBJenkinsDocker/SbJenkinsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.SBJenkinsDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbJenkinsDockerApplication.class, args);
+	}
+
+}
---
+++ SBJenkinsDocker/src/test/java/com/example/SBJenkinsDocker/SbJenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SBJenkinsDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbJenkinsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
